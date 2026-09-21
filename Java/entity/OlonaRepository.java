b96aa390d45a0458f9b2dac9ab3a74978c2a8b78
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 14:12:46 +0300

    Add reader function

--- Java/entity/OlonaRepository.java
+++ Java/entity/OlonaRepository.java
@@ -1,5 +1,30 @@
 package entity;
+import java.util.Array;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class OlonaRepository {
-    
+    String cheminFichier;
+    String separateur;
+
+    List<Olona> readThem() {
+        List<Olona> listOlona = new ArrayList<>();
+
+        // Fonction pour lire le fichier 
+        try () {
+            FileReader fileReader = new FileReader(this.cheminFichier);
+            BufferedReader reader = new BufferedReader(fileReader);
+            // Debut de la boucle 
+            
+            // À chaque ligne , transforme en un objet et le met dans la liste 
+
+            // Fin de la boucle 
+        } catch (IOException e) {
+
+        }
+
+        // Liberer de la mémoire 
+    }
 }
