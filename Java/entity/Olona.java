b11a1313976ff6b3fcf933d387feb754d0eda9cd
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 14:26:22 +0300

    Add getters and setters on Olona

--- Java/entity/Olona.java
+++ Java/entity/Olona.java
@@ -7,4 +7,39 @@ public class Olona {
     String email;
 
     public Olona(); 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // NOM
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    // PRENOM
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    // EMAIL
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
