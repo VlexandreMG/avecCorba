4c05fc2c53eb0f5a52ce14a6d4342d48c7e99cf6
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 21:37:13 +0300

    Add getters and setters

--- C++/OlonaRepository.hpp
+++ C++/OlonaRepository.hpp
@@ -12,4 +12,16 @@ public:
     std::string user;
     std::string password;
     std::string database;
+
+    std::string getHost() const;
+    void setHost(const std::string& host);
+
+    std::string getUser() const;
+    void setUser(const std::string& user);
+
+    std::string getPassword() const;
+    void setPassword(const std::string& password);
+
+    std::string getDatabase() const;
+    void setDatabase(const std::string& database);
 };
