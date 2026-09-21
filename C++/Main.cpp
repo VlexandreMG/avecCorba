766fa27195588afe03ac0613cd709344edea30c6
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 23:04:07 +0300

    Done Main.cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -6,4 +6,15 @@ int main() {
     OlonaRepository repo;
 
     std::vector<Olona> listeOlona = repo.readThem();
+
+    if (listeOlona.empty()) {
+        std::cout << "Aucun olona trouvé dans la base" << std::endl;
+    } else {
+        for (const auto& olona : listeOlona) {
+            std::cout << "Id : " << olona.getId()
+                      << "Nom : " << olona.getNom()
+                      << "Prenom : " << olona.getPrenom()        
+                      << "Email : " << olona.getEmail() << std::endl;
+        }
+    }
 }
