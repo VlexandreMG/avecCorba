89e1b6e769d7731ce4929bd811c6c9925110da9d
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 22:48:49 +0300

    Add body of function

--- C++/OlonaRepository.cpp
+++ C++/OlonaRepository.cpp
@@ -21,5 +21,39 @@ std::string OlonaRepository::getDatabase() const { return database; }
 void OlonaRepository::setDatabase(const std::string& db) { database = db; }
 
 std::vector<Olona> OlonaRepository::readThem() {
+    std::vector<Olona> listOlona;
 
+    try {
+        // Activation du driver 
+        sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+
+        // Connexion à la base de données 
+        std::unique_ptr<sql::Connection> con(driver->connect(host, user, password));
+
+        // Selectionner la base de données 
+        con->setSchema(database);
+
+        // Création du statement 
+        std::unique_ptr<sql::Statement> stmt(con->createStatement());
+
+        // Execution de la requête SQL 
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT id, nom, prenom, email FROM olona"));
+
+        // Parcours du ResultSet 
+        while (res->next()) {
+            int id = res->getInt("id");
+            std::string nom = res->getString("nom");
+            std::string prenom = res->getString("prenom");
+            std::string email = res->getString("email");
+
+            // Instanciation de l'objet et ajout dans le vecteur
+            Olona olona{id , nom , prenom , email};
+            listOlona.push_back(olona);
+        }
+    } catch (sql::SQLException& e) {
+        std::cerr << "Erreur de connexion MySQL : " << e.what() << std::endl;
+        std::cerr << "Code d'erreur MySQL : " << e.getErrorCode() << std::endl;
+    }
+
+    return listOlona;
 }
