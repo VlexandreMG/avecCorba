3f28f2507854d8ecae1710901ee3f5058bffefd8
VlexandreMG <dodolashmou@gmail.com>
2026-09-21 23:16:51 +0300

    Add getters and setters

--- C++/entity/Olona.hpp
+++ C++/entity/Olona.hpp
@@ -6,4 +6,16 @@ public:
     String nom;
     String prenom;
     String email; 
+
+    std::int getId(); 
+    void setId(std::int& id);
+
+    std::string getNom(); 
+    void setNom(std::string& nom);
+
+    std::string getPrenom(); 
+    void setPrenom(std::string& prenom);
+
+    std::string getEmail(); 
+    void setEmail(std::string& email);
 };
