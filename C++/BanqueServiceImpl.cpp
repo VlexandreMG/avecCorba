aba28fc12a5266df767b6cbdd4b735b44152c753
VlexandreMG <dodolashmou@gmail.com>
2026-09-23 04:23:03 +0300

    Add implement file

---
+++ C++/BanqueServiceImpl.cpp
@@ -0,0 +1,8 @@
+#include "BanqueServiceImpl.hh"
+
+BanqueServiceImpl::BanqueServiceImpl() {}
+BanqueServiceImpl::~BanqueServiceImpl() {}
+
+::CORBA::Boolean faireDepot(::CORBA::Long idCompte , ::CORBA::Double montant) {
+    
+}
