e63d8ddda5a656493471c086225adf20b5bdaf54
VlexandreMG <dodolashmou@gmail.com>
2026-09-23 04:17:38 +0300

    Add fully documentary

--- C++/BanqueServiceImpl.hh
+++ C++/BanqueServiceImpl.hh
@@ -1,14 +1,22 @@
 #pragma once 
 
+#include <iostream>
 #include "banque.hh"
 
-// C'est quoi le POA  
+// C'est quoi le POA : Le portable indique la portabilité sur n'importe 
+// quel ORB , cet héritage transforme la classe BanqueServiceImpl en skeleton
+// pour qu'il puisse intéragir en réseau. 
+
 class BanqueServiceImpl : public POA_BanqueModule::banqueService {
 public:
     BanqueServiceImpl();
+
     // Pourquoi il y a un constructeur en virtual 
+    // ~ signifie que c'est un destructeur , et le virtual ,
+    // lorsque CORBA détruit l'objet , le destructeur de la classe fille est executé en premier.  
     virtual ~BanqueServiceImpl();
 
     // Pourquoi il y a tous ces CORBA 
+    // Pour éviter les conflits avec les noms 
     ::CORBA::Boolean faireDepot(::CORBA::Long idCompte , ::CORBA::Double montant) override;
 };
