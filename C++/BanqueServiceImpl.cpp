dc353612502dcb55877f0b5cd6ff7087beadbab3
VlexandreMG <dodolashmou@gmail.com>
2026-09-23 04:28:11 +0300

    Add print only

--- C++/BanqueServiceImpl.cpp
+++ C++/BanqueServiceImpl.cpp
@@ -4,5 +4,10 @@ BanqueServiceImpl::BanqueServiceImpl() {}
 BanqueServiceImpl::~BanqueServiceImpl() {}
 
 ::CORBA::Boolean faireDepot(::CORBA::Long idCompte , ::CORBA::Double montant) {
+    std::cout << "[SERVEUR C++] Id Compte : "<< idCompte << std::endl;
+    std::cout << "[SERVEUR C++] Montant : "<< montant << std::endl;
     
+    
+    std::cout << "[SERVEUR C++] Depot effectue avec succes !" << std::endl;
+    return true;
 }
