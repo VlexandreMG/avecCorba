9a9d1fc750f4b498282bd32cb551205e8b326e17
VlexandreMG <dodolashmou@gmail.com>
2026-09-23 08:27:36 +0300

    Allumer le bus CORBA

---
+++ C++/Server.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <omniORB4/CORBA.h>
+#include <naming/Naming.hh>
+#include "BanqueServiceImpl.hh"
+
+int main(int argc , char**argv) {
+    try {
+
+    // Allumer l'ORB 
+
+    // Activer le POA
+    
+    // Instancier le service C++ 
+
+    // Trouver le nameservice 
+
+    // Enregistrer l'objet sous "BanqueService"
+
+    // Bloquer sur orb->run()
+    }
+    catch () {
+        
+    }
+}
