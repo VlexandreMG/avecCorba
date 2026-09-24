281780956612811080b86f8eeacd8d1490acdc73
VlexandreMG <dodolashmou@gmail.com>
2026-09-24 04:46:06 +0300

    Last issues

--- C++/src/BanqueServiceImpl.hh
+++ C++/src/BanqueServiceImpl.hh
@@ -16,9 +16,9 @@ public:
     // Pourquoi il y a un constructeur en virtual 
     // ~ signifie que c'est un destructeur , et le virtual ,
     // lorsque CORBA détruit l'objet , le destructeur de la classe fille est executé en premier.  
-    virtual ~BanqueServiceImpl();
+    // virtual ~BanqueServiceImpl();
 
     // Pourquoi il y a tous ces CORBA 
     // Pour éviter les conflits avec les noms 
-    ::CORBA::Boolean faireDepot(::CORBA::Long idCompte , ::CORBA::Double montant) override;
+    Banque::Compte faireDepot(::CORBA::Long idCompte , ::CORBA::Double montant) override;
 };
