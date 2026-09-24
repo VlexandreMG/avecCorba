d2804c95f5e4bab3d1ad2b11570677e05033fd1f
VlexandreMG <dodolashmou@gmail.com>
2026-09-24 08:38:51 +0300

    Add serviceImpl file

---
+++ Java/src/service/BanqueServiceImpl.java
@@ -0,0 +1,23 @@
+package service;
+
+import Banque.BanqueServicePOA;
+import Banque.Compte;
+
+public class BanqueServiceImpl extends BanqueServicePOA {
+
+    @Override
+    public Compte faireDepot(int idCompte, double montant) {
+        System.out.println("[Java Server] Requête de dépôt reçue ! ID: " + idCompte + " | Montant: " + montant);
+
+        // Exemple de logique côté Java :
+        // Ici, vous pouvez traiter les données, interroger une BDD ou instancier le compte
+        Compte compte = new Compte();
+        compte.id = idCompte;
+        compte.nom = "Client Java Server";
+        compte.solde = 150000.0 + montant; // Exemple de solde mis à jour
+
+        System.out.println("[Java Server] Traitement effectué. Nouveau solde : " + compte.solde);
+        
+        return compte;
+    }
+}
