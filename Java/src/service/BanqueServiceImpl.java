abbdd3f82409e9f00f4489c0259de09233bfaeb9
VlexandreMG <dodolashmou@gmail.com>
2026-09-24 11:44:36 +0300

    Add and function with BDD in faireDepot java Server

--- Java/src/service/BanqueServiceImpl.java
+++ Java/src/service/BanqueServiceImpl.java
@@ -2,22 +2,32 @@ package service;
 
 import Banque.banqueServicePOA;
 import Banque.Compte;
+import entity.CompteRepository;
 
 public class BanqueServiceImpl extends banqueServicePOA {
+
+    CompteRepository cpRep;
 
     @Override
     public Compte faireDepot(int idCompte, double montant) {
         System.out.println("[Java Server] Requête de dépôt reçue ! ID: " + idCompte + " | Montant: " + montant);
 
-        // Exemple de logique côté Java :
-        // Ici, vous pouvez traiter les données, interroger une BDD ou instancier le compte
-        Compte compte = new Compte();
-        compte.id = idCompte;
-        compte.nom = "Client Java Server";
-        compte.solde = 150000.0 + montant; // Exemple de solde mis à jour
+        cpRep.setCheminFichier("donnees.txt");
+        cpRep.setSeparateur("\\;");
 
-        System.out.println("[Java Server] Traitement effectué. Nouveau solde : " + compte.solde);
-        
-        return compte;
+        if (!cpRep.updateCompte(idCompte, montant)) {
+            System.out.println("Erreur dans l'update");
+        } else {
+            Compte compte = cpRep.selectCompte(idCompte);
+            return compte;
+        }
+
+        // // Exemple de logique côté Java :
+        // // Ici, vous pouvez traiter les données, interroger une BDD ou instancier le compte
+        // Compte compte = new Compte();
+        // compte.id = idCompte;
+        // compte.nom = "Client Java Server";
+        // compte.solde = 150000.0 + montant; // Exemple de solde mis à jour
+        return null;
     }
 }
