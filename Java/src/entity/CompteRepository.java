3863d8d497ed4d8508731c373aa66c746c55b30c
VlexandreMG <dodolashmou@gmail.com>
2026-09-24 03:39:54 +0300

    Debugging getCompteFromCpp

--- Java/src/entity/CompteRepository.java
+++ Java/src/entity/CompteRepository.java
@@ -6,6 +6,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import Banque.Compte;
+import Banque.BanqueService;
+
 public class CompteRepository {
     String cheminFichier;
     String separateur;
@@ -29,40 +32,58 @@ public class CompteRepository {
         this.separateur = separateur;
     }
 
-    public List<Compte> readThem() {
-        List<Compte> listCompte = new ArrayList<>();
+    // public List<Compte> readThem() {
+    //     List<Compte> listCompte = new ArrayList<>();
 
-        // Fonction pour lire le fichier 
-        try (FileReader fileReader = new FileReader(this.cheminFichier);) {
-            BufferedReader reader = new BufferedReader(fileReader);
+    //     // Fonction pour lire le fichier 
+    //     try (FileReader fileReader = new FileReader(this.cheminFichier);) {
+    //         BufferedReader reader = new BufferedReader(fileReader);
 
-            String line;
-            // Debut de la boucle 
-            while ((line = reader.readLine()) != null) {
-            // À chaque ligne , transforme en un objet et le met dans la liste 
-                if (!line.trim().isEmpty()) {
-                    String[] champs =  line.split(this.separateur);
+    //         String line;
+    //         // Debut de la boucle 
+    //         while ((line = reader.readLine()) != null) {
+    //         // À chaque ligne , transforme en un objet et le met dans la liste 
+    //             if (!line.trim().isEmpty()) {
+    //                 String[] champs =  line.split(this.separateur);
 
-                    int id = Integer.parseInt(champs[0]);
-                    String nom = champs[1];
+    //                 int id = Integer.parseInt(champs[0]);
+    //                 String nom = champs[1];
 
-                    Compte man = new Compte();
-                    man.setId(id);
-                    man.setNom(nom);
+    //                 Compte man = new Compte();
+    //                 man.setId(id);
+    //                 man.setNom(nom);
 
-            // Ajout dans la liste 
-            listCompte.add(man);
-                }
+    //         // Ajout dans la liste 
+    //         listCompte.add(man);
+    //             }
 
-            // Fin de la boucle 
-            }
-        } catch (IOException e) {
-            // Liberer de la mémoire 
+    //         // Fin de la boucle 
+    //         }
+    //     } catch (IOException e) {
+    //         // Liberer de la mémoire 
+    //         e.printStackTrace();
+    //     }
+
+    //     return listCompte;
+    // }
+
+    public Compte getCompteFromCpp (BanqueService serviceCorba, int id, double montant) {
+        Compte compteMisAJour = null;
+
+        try {
+            // Appel distant de la méthode faireDepot de BanqueServiceImpl en C++
+            compteMisAJour = serviceCorba.faireDepot(id, montant);
+            
+            System.out.println("[Java Client] Compte reçu du C++ :");
+            System.out.println("  ID     : " + compteMisAJour.id);
+            System.out.println("  Nom    : " + compteMisAJour.nom);
+            System.out.println("  Solde  : " + compteMisAJour.solde);
+
+        } catch (Exception e) {
+            System.err.println("[Java Client] Erreur lors de l'appel CORBA : " + e.getMessage());
             e.printStackTrace();
         }
 
-        return listCompte;
-    }
-
-    
+        return compteMisAJour;
+    }   
 }
